efa07da77a939181ab077f7e6dc20fdd8628f748
Daniel Arthur Gallagher <8526031+danieleloscozzese@users.noreply.github.com>
2024-12-16 09:51:40 +0100

    docs: expose JSDoc on the type declaration
    VS Code, for example, does not read the comments of the source

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
 declare module "post-task" {
+	/**
+	 * Queues an arbitrary task to be executed in the browser, with the given priority.
+	 * Allows breaking up the work of potentially long-running tasks to avoid blocking the main thread.
+	 */
 	export default function postTask(
 		task: () => void,
 		priority: "background" | "user-visible" | "user-blocking",
