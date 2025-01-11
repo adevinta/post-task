b2e47f4fba08bb376470775ee442e6c5722507a9
Daniel Arthur Gallagher <daniel.gallagher@adevinta.com>
2025-01-11 18:36:59 +0100

    refactor: adjust the fallbacks Remove the idle callback which was misused Add a microtask alternative for user-blocking tasks

--- index.mjs
+++ index.mjs
@@ -5,40 +5,44 @@
  * The priority of the task: these are the priorities of the Scheduler API.
  * @typedef {('background' | 'user-visible' | 'user-blocking')} SchedulerPriority
  *
- * The timeouts used for setTimeout, which define the minimum delay in
- * milliseconds before the microtask is executed.
+ * The timeouts used for `setTimeout`, which define the minimum delay in
+ * milliseconds before the callback will be executed.
  * @type {Record<SchedulerPriority, number>}
  */
 const priorityCallbackDelays = Object.create(null, {
 	/**
-	 * 150ms is the threshold for a "long task" in Chrome. Waiting at least that
-	 * long will give a better chance for long queues of work to be broken up.
+	 * A 150ms duration defines a "long task" for the Web Vitals.
+	 * Waiting at least that long will give a better chance for long queues of
+	 * work to be broken up.
+	 *
 	 * The task will then be scheduled as part of the next event loop, but
 	 * ideally without directly adding to congestion if the CPU is busy.
 	 */
-	background: { value: 150, enumerable: true },
+	background: { value: 150 },
 	/**
-	 * The 10ms are arbitrary, chosen because it's an imperceptible delay.
-	 * However this approximates a lower priority than `"user-blocking"`.
+	 * User-visible tasks are scheduled immediately, and although this is the
+	 * same timeout as for `"user-blocking"`, the fallback for that case will
+	 * almost always schedule a microtask with the `queueMicrotask` function;
+	 * while the 0ms timeout here will schedule a **macro**task which will run
+	 * with a lower priority in the event loop.
 	 */
-	"user-visible": { value: 10, enumerable: true },
+	"user-visible": { value: 0 },
 	/**
-	 * User-blocking callbacks are scheduled immediately.
+	 * User-blocking callbacks are scheduled immediately, mirroring the
+	 * user-visible case as a fallback for the unlikely case that
+	 * `queueMicrotask` is not available.
 	 */
-	"user-blocking": { value: 0, enumerable: true },
+	"user-blocking": { value: 0 },
 });
 
-/** @typedef {() => void} Task */
-
 /**
- * Queues an arbitrary task to be executed in the browser, with the given
+ * Queues an arbitrary task to be scheduled for execution with the given
  * priority.
  *
  * Allows the discrete and prioritised queuing of tasks which if run serially
- * would block the main thread, but which do not have to be run immediately nor
- * in a fully-blocking way.
+ * would block the main thread, but which do not have to be run immediately.
  *
- * @param {Task} task The callback to be executed.
+ * @param {() => void} task The callback to be executed.
  * @param {SchedulerPriority} priority The priority of the task, following the
  * Scheduler API.
  * @returns {Promise<void>} A promise that resolves when the task is executed,
@@ -46,12 +50,23 @@ const priorityCallbackDelays = Object.create(null, {
  */
 const postTask = (task, priority) => {
 	// Prefer to use the Scheduler API, if available.
-	if (typeof window !== "undefined" && "scheduler" in window) {
-		return scheduler.postTask(task, {
+	if ("scheduler" in globalThis) {
+		return globalThis.scheduler.postTask(task, {
 			priority,
 		});
 	}
-	// Otherwise, or on Node.js, set a timeout with the appropriate delay
+	// Otherwise, if available and for user-blocking tasks,
+	// use the native `queueMicrotask`.
+	else if (priority === "user-blocking" && "queueMicrotask" in globalThis) {
+		return new Promise((resolve) => {
+			globalThis.queueMicrotask(() => {
+				task();
+				resolve();
+			});
+		});
+	}
+	// Otherwise, and always for the lower priorities on Node.js where the
+	// Scheduler API is not available, set a timeout with the appropriate delay.
 	else {
 		return new Promise((resolve) => {
 			setTimeout(() => {
